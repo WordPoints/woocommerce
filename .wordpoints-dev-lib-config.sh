0750ba7e95f56aa0a2ff9f72cd54e52a3933ab4a
JDGrimes <jdg@codesymphony.co>
2016-08-09 11:40:35 -0400

    Travis: install WooCommerce correctly
    See #17

--- .wordpoints-dev-lib-config.sh
+++ .wordpoints-dev-lib-config.sh
@@ -6,10 +6,12 @@ shopt -s expand_aliases
 # Install WooCommerce when running tests.
 install-woocommerce() {
 
+    mkdir /tmp/woocommerce
+
 	curl -L "https://github.com/woothemes/woocommerce/archive/${WC_VERSION}.tar.gz" \
 		| tar xvz --strip-components=1 -C /tmp/woocommerce
 
-	mv /tmp/woocommerce/src /tmp/wordpress/src/wp-content/plugins/woocommerce
+	mv /tmp/woocommerce /tmp/wordpress/src/wp-content/plugins/woocommerce
 }
 
 # Sets up custom configuration.
