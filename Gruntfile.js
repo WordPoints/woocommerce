143a3f7a0f80d1294021115d1d4bfa677dfac647
JDGrimes <jdg@codesymphony.co>
2017-02-02 14:03:07 -0500

    Sync Grunt config with dev-lib
    Update the dev-lib, sync the config, and add the `yarn.lock` file.

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+/**
+ * Grunt configuration file.
+ *
+ * @package WordPoints_Dev_Lib
+ * @since 2.5.0
+ */
+
+/* jshint node:true */
+module.exports = function( grunt ) {
+
+	// Load the default configuration from the dev-lib.
+	require( './dev-lib/grunt/modules/configure.js' )( grunt, __dirname );
+};
+
+// EOF
