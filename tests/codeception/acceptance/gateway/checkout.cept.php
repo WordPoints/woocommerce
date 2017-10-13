2d241a2b5ae5cf566cf00db15d10ffe516dba970
JDGrimes <jdg@codesymphony.co>
2017-10-13 16:48:37 -0400

    Add the wait check in the correct spot

--- tests/codeception/acceptance/gateway/checkout.cept.php
+++ tests/codeception/acceptance/gateway/checkout.cept.php
@@ -43,6 +43,7 @@ $I->loginAs( 'customer', 'password' );
 $I->amOnPage( str_replace( home_url(), '', get_permalink( $product_id ) ) );
 $I->click( 'Add to cart' );
 $I->amOnPage( str_replace( home_url(), '', wc_get_page_permalink( 'checkout' ) ) );
+$I->waitForElement( '.woocommerce' );
 $I->click( '#payment_method_wordpoints_points' );
 $I->waitForElementVisible( '[name=wordpoints_points-points-type]' );
 $I->selectOption( 'wordpoints_points-points-type', 'Test' );
