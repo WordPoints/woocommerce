673d5971b36d8778a1613a9d103b71e075b31b53
JDGrimes <jdg@codesymphony.co>
2018-02-09 17:16:38 -0500

    Introduce points-deducting coupons
    Allows a coupon to cost a certain number of points per use.
    
    Fixes #6

--- tests/codeception/acceptance/bootstrap.php
+++ tests/codeception/acceptance/bootstrap.php
@@ -21,4 +21,11 @@ require_once __DIR__ . '/../../phpunit/includes/factories/product.php';
  */
 require_once __DIR__ . '/../../phpunit/includes/factories/order.php';
 
+/**
+ * Load the WooCommerce tests bootstrap.
+ *
+ * @since 1.3.0
+ */
+require_once WP_PLUGIN_DIR . '/woocommerce/tests/bootstrap.php';
+
 // EOF
---
+++ tests/codeception/acceptance/coupon/settings.cept.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Tests saving the coupon settings.
+ *
+ * @package WordPoints_WooCommerce
+ * @since   1.3.0
+ */
+
+$I = new AcceptanceTester( $scenario );
+$I->wantTo( 'Save coupon points settings.' );
+$I->hadCreatedAPointsType();
+$I->amLoggedInAsAdminOnPage(
+	'/wp-admin/post-new.php?post_type=shop_coupon'
+);
+$I->fillField( 'coupon_amount', '10' );
+$I->click( 'Points', '.coupon_data_tabs' );
+$I->fillField( 'wordpoints_points_amount', '10' );
+$I->click( 'Publish' );
+$I->click( 'Points', '.coupon_data_tabs' );
+$I->seeInField( 'wordpoints_points_amount', '10' );
+
+// EOF
